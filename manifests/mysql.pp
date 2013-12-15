7535b8f7571466c562891af1d5eb99c8888f865a
Andrew Leonard <andy@hurricane-ridge.com>
2013-12-15 07:21:34 -0800

    Add MySQL database and grant creation.

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -4,18 +4,33 @@
 #
 # === Parameters
 #
+# [*databases*]
+#   Hash of database hashes for mysql_database provider.
+#
+# [*grants*]
+#   Hash of grant hashes for mysql_grant provider.
+#
 # [*users*]
 #   Hash of user hashes for mysql_user provider.
 #
 # === Examples
 #
 # class { '::databases::mysql':
-#   users => {
+#   databases => {
+#     'fizzle' => {
+#       charset => 'utf8'
+#     }
+#   },
+#   grants    => {
+#     'foo@localhost/fizzle.*' => {
+#       privileges => ['ALL'],
+#       table      => 'fizzle.*',
+#       user       => 'foo@localhost',
+#     }
+#   },
+#   users     => {
 #     'foo@localhost' => {
 #       password_hash => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF'
-#     },
-#     'bar@localhost' => {
-#       password_hash => '*E8D46CE25265E545D225A8A6F1BAF642FEBEE5CB'
 #     }
 #   }
 # }
@@ -29,9 +44,20 @@
 # Copyright 2013 Andrew Leonard
 #
 class databases::mysql(
+  $databases = {},
+  $grants = {},
   $users = {}
   ) {
 
   create_resources(mysql_user, $users)
+  create_resources(mysql_database, $databases)
+
+  # If mysql_grant is run before the user is created with mysql_user,
+  # the user will automatically be created.  (NO_AUTO_CREATE_USER
+  # appears to be off by default.)  This exposes a race condition
+  # before the user's password is set by mysql_user().  Therefore, it
+  # may be desirable to find a way to chain these resources
+  # appropriately.
+  create_resources(mysql_grant, $grants)
 
 }
--- vagrant/vagrant.pp
+++ vagrant/vagrant.pp
@@ -1,8 +1,27 @@
-include ::mysql::server
+class { '::mysql::server':
+  remove_default_accounts => true,
+} ->
 
 # Include this module
 class { '::databases::mysql':
-  users => {
+  databases => {
+    'fizzle' => {
+      charset => 'utf8'
+    }
+  },
+  grants    => {
+    'foo@localhost/fizzle.*' => {
+      privileges => ['ALL'],
+      table      => 'fizzle.*',
+      user       => 'foo@localhost',
+    },
+    'bar@localhost/fizzle.*' => {
+      privileges => ['ALL'],
+      table      => 'fizzle.*',
+      user       => 'bar@localhost',
+    }
+  },
+  users     => {
     'foo@localhost' => {
       password_hash => '*F3A2A51A9B0F2BE2468926B4132313728C250DBF'
     },
